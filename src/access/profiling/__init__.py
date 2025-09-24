56aa56a3ec8d871b97fe47a6e1f4f0c1d33b2a31
Manodeep Sinha <manodeep@gmail.com>
2025-09-24 14:13:42 +1000

    Cleaning up commits

--- src/access/profiling/__init__.py
+++ src/access/profiling/__init__.py
@@ -13,7 +13,14 @@ except PackageNotFoundError:
 
 from access.profiling.parser import ProfilingParser
 from access.profiling.fms_parser import FMSProfilingParser
+from access.profiling.um_parser import UMProfilingParser
 from access.profiling.payujson_parser import PayuJSONProfilingParser
 from access.profiling.cice5_parser import CICE5ProfilingParser
 
-__all__ = ["ProfilingParser", "FMSProfilingParser", "PayuJSONProfilingParser", "CICE5ProfilingParser"]
+__all__ = [
+    "ProfilingParser",
+    "FMSProfilingParser",
+    "UMProfilingParser",
+    "CICE5ProfilingParser",
+    "PayuJSONProfilingParser",
+]
