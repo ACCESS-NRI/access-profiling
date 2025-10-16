be1b0516c9acc6974c73d559c1e670ba2c1b6c89
Micael Oliveira <micael.oliveira@anu.edu.au>
2025-10-16 11:16:34 +1100

    Remove parse_data_series from ProfilingParser, as that type of operations (concatenating profiling data) should be handled by the ProfilingConfig class.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -53,32 +53,6 @@ def test_base_parser(profiling_data, parser):
         assert parser.read(stream) == profiling_data[stream], f'Incorrect profiling stats returned for "{stream}"'
 
 
-def test_parse_data_series(profiling_data, parser):
-    """Tests the parse_data_series method of ProfilingParser."""
-    dataset = parser.parse_data_series(["1cpu_stream", "2cpu_stream"], "ncpus", [1, 2])
-
-    assert set(dataset.dims) == {"ncpus", "region"}, "Dataset should have dimensions 'ncpus' and 'region'!"
-    assert dataset.sizes["ncpus"] == 2, "Dataset should have 2 values for 'ncpus'!"
-    assert dataset.sizes["region"] == 3, "Dataset should have 3 values for 'region'!"
-    assert set(dataset.data_vars) == set(parser.metrics), "Dataset should have data_vars for each metric!"
-    assert all(dataset[metric].shape == (2, 3) for metric in parser.metrics), (
-        "Dataset data vars should have shape (2, 3)!"
-    )
-    assert all(dataset[metric].data.units == metric.units for metric in parser.metrics), (
-        "Dataset data_vars should have correct units!"
-    )
-    assert all(
-        dataset[metric].sel(ncpus=1).pint.dequantify().values.flatten().tolist()
-        == profiling_data["1cpu_stream"][metric]
-        for metric in parser.metrics
-    ), "Dataset data vars should have correct values for ncpus=1!"
-    assert all(
-        dataset[metric].sel(ncpus=2).pint.dequantify().values.flatten().tolist()
-        == profiling_data["2cpu_stream"][metric]
-        for metric in parser.metrics
-    ), "Dataset data vars should have correct values for ncpus=2!"
-
-
 def test_str2num():
     """Tests conversion of numbers to most appropriate type."""
     str2int = _convert_from_string("42")
